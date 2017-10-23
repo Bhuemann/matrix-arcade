7dbd3500d82f86c79c40cbc7843ef731b299e82c
bhuemann <benhuemann@gmail.com>
2017-10-23 23:51:17 +0000

    added message queue header

---
+++ headers/msgque.h
@@ -0,0 +1,34 @@
+#ifndef MSGQUE_H
+#define MSGQUE_H
+
+#include "gamepadhandler.h"
+
+#define MQ_NAME "/mq_gp_event" 
+
+//add warnings here
+#define WARNING_GP_DISCONECT         0x01;
+
+
+enum {
+	DATA_TYPE_INIT,
+	DATA_TYPE_WARNING,
+	DATA_TYPE_EVENT
+};
+
+typedef union {
+
+	button_event_t event;
+	unsigned char warnmsg;
+	js_attr_t init;
+
+} mq_data_t;
+
+typedef struct {
+
+	int type;
+	mq_data_t data;
+	
+}mq_msg_t;
+
+
+#endif
